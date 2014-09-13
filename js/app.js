99cba8ee4cd10e110da026a0587230a68a4efc9d
Billy Onjea <vasili.onjea@gmail.com>
2014-09-12 20:48:18 -0400

    adding in various utils, fix hover state in projects so only one is active, general cleanups

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 var IC = {};
 
 // Utils
+// -----------------------------
 var 
   win = window,
   doc = win.document,
@@ -69,7 +70,9 @@ var
   };
 
 
+
 // Main Navigation
+// -----------------------------
 var 
   navToggle = util.find('.nav-toggle-mobile'),
   mobileNav = util.find('.mobile-nav');
@@ -83,7 +86,9 @@ var
   navToggle.addEventListener('click', toggleTopNav, false);
 
 
+
 // Recent Projects
+// -----------------------------
 var 
   recentProjects = util.find('.recent-projects'),
   originalActive = util.find('.project.active', recentProjects),
@@ -106,120 +111,4 @@ var
     projects.forEach(function(el) { el && el.classList.remove('active'); });
     originalActive.classList.add('active');
   });
-
-
-
-
-  var css = {
-    get: function(elem, prop) {
-      return doc.defaultView.getComputedStyle(elem).getPropertyValue(prop);
-    },
-
-    set: function(elem, props) {
-      for (var p in props) {
-        if (props.hasOwnProperty(p)) {
-          elem.style[p] = props[p];
-        }
-      }
-    },
-
-    width: function(elem) {
-      return elem.getBoundingClientRect().width;
-    },
-
-    height: function(elem) {
-      return elem.getBoundingClientRect().height;
-    },
-
-    position: function(elem) {
-      if (!elem) { return; }
-
-      var rect = elem.getBoundingClientRect();
-
-      return {
-        top: rect.top,
-        right: rect.right,
-        bottom: rect.bottom,
-        left: rect.left
-      };
-    }
-  };
-
-
-  var events = {
-    add: function(elem, type, fn) {
-      return elem.addEventListener(type, fn, false);
-    },
-
-    remove: function(elem, type, fn) {
-      return elem.removeEventListener(type, fn, false);
-    }
-  };
-
-
-
-  IC.siteHeader = (function() {
-  var 
-    header = doc.getElementById('site-header'),
-    size = 1.25, a = 0,
-    bodyFontSize = Number(css.get(doc.body, 'font-size').slice(0, -2)),
-    headFontSize = Number(css.get(header, 'font-size').slice(0, -2)) / bodyFontSize,
-    headerHeight = css.height(header),
-    currentSize = a,
-    lastSize = 0;
-
-    // css.set(header, {'font-size': size+'em'});
-    
-    // events.add(win, 'scroll', function() {
-    //   if (win.scrollY - headerHeight >= headerHeight * 0.75) {
-    //     header.classList.remove('grow-header');
-    //   } else {
-    //     header.classList.add('grow-header');
-    //   }
-
-    //   return;
-    // });
-  }).call(IC);
-
-
-
-  // Leadertext
-  // var leadertextItems = util.findAll('.leadertext-item');
-  // var time = 500;
-
-  // win.setTimeout(function() {
-  //   if (leadertextItems.length) {
-  //     [].forEach.call(leadertextItems, function(el, i) {
-  //       if (i !== 0) {
-  //         time += (time*i)/4;
-  //       }
-  //       win.setTimeout(function() { leadertextItems[i].classList.add('slide-from-right'); }, time );
-  //     });
-  //   }
-  // }, 1500);
-
-
-  // Parallax fade.
-  // var banner = util.find('.leaderboard');
-  // var bannerTop = Math.abs(banner.getBoundingClientRect().top); // about 50
-  // var bannerHeight = (banner.getBoundingClientRect().height + bannerTop) * .58;
-  // var inner = banner.querySelector('.inner');
-
-  // function changeOpacity(el, val) {
-  //   el.style.opacity = val;
-  // }
-
-  // win.addEventListener('scroll', function(e) {
-  //   var curPos = (doc.body.scrollTop/bannerHeight).toFixed(2);
-    
-  //   if (curPos < 1 && curPos >= 0) {
-  //     // console.log('I have scrolled ' + (curPos*100).toFixed(2) + ' percent in relation to the banner.');
-  //     if (curPos === 0) {
-  //       changeOpacity(inner, 1);
-  //       return;
-  //     }
-
-  //     changeOpacity(inner, (1 - curPos).toFixed(2));
-  //   }
-  // }, false);
 })(this);
